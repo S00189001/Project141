0e25e37abfad796b8e59c5bc79bfb80acd0e59f9
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2021-07-02 16:14:56 +0100

    Added Code for InGameEscMenuKeybinds
    - Will need this for the action bars binds

--- UE_MultiplayerTemplate/Source/UE_MultiplayerTemp/MenuSystem/InGameEscMenu.cpp
+++ UE_MultiplayerTemplate/Source/UE_MultiplayerTemp/MenuSystem/InGameEscMenu.cpp
@@ -2,6 +2,8 @@
 
 
 #include "UE_MultiplayerTemp/MenuSystem/InGameEscMenu.h"
+
+#include "Components/WidgetSwitcher.h"
 #include "Components/Button.h"
 
 bool UInGameEscMenu::Initialize()
@@ -13,6 +15,15 @@ bool UInGameEscMenu::Initialize()
 	// Resume Button
 	if (!ensure(EscMenuResumeButton != nullptr)) return false;
 	EscMenuResumeButton->OnClicked.AddDynamic(this, &UInGameEscMenu::ResumePressed);
+
+	// Keybinds Button
+	if (!ensure(EscMenuKeybindsButton != nullptr)) return false;
+	EscMenuKeybindsButton->OnClicked.AddDynamic(this, &UInGameEscMenu::OpenKeybindsMenu);
+
+	// Settings Button
+	if (!ensure(EscMenuSettingsButton != nullptr)) return false;
+	EscMenuSettingsButton->OnClicked.AddDynamic(this, &UInGameEscMenu::SettingsPressed);
+
 
 	// Exit Button
 	if (!ensure(EscMenuExitButton != nullptr)) return false;
@@ -27,6 +38,16 @@ void UInGameEscMenu::ResumePressed()
 	MenuTeardown();
 }
 
+void UInGameEscMenu::KeybindsPressed()
+{
+
+}
+
+void UInGameEscMenu::SettingsPressed()
+{
+	
+}
+
 void UInGameEscMenu::ExitPressed()
 {
 	if (P_MenuInterface != nullptr)
@@ -34,4 +55,15 @@ void UInGameEscMenu::ExitPressed()
 		MenuTeardown();
 		P_MenuInterface->LoadMainMenu();
 	}
+}
+
+void UInGameEscMenu::OpenKeybindsMenu()
+{
+	if (!ensure(EscMenuSwitcher != nullptr)) return;
+	if (!ensure(KeybindsMenu != nullptr)) return;
+	EscMenuSwitcher->SetActiveWidget(KeybindsMenu);
+}
+
+void UInGameEscMenu::OpenInGameEscMenu()
+{
 }
