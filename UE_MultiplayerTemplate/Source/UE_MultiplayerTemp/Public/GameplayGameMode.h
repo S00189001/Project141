e4435eed48386e2453971d0a86d7b44404701466
SimranDhillon20 <S00187359@mail.itsligo.ie>
2021-07-14 00:37:22 +0100

    created new Gamemode and player controller

---
+++ UE_MultiplayerTemplate/Source/UE_MultiplayerTemp/Public/GameplayGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GameplayGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_MULTIPLAYERTEMP_API AGameplayGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
